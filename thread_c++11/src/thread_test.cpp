766f45f9e219e852bdc537bdf385fe3dc5e47d9c
Yunfeng Hu <yhu@synaptics.com>
2018-10-07 20:46:54 -0700

    update file

---
+++ thread_c++11/src/thread_test.cpp
@@ -0,0 +1,20 @@
+﻿#include <thread>  
+#include <chrono>
+#include <iostream>
+#include<array> 
+using namespace std;
+
+void show()
+{
+    cout << "hello cplusplus!" << endl;
+}
+int main()
+{
+    array<thread, 3>  threads = { thread(show), thread(show), thread(show) };
+    for (int i = 0; i < 3; i++)
+    {
+        cout << threads[i].joinable() << endl;
+        threads[i].join();
+    }
+    return 0;
+}
