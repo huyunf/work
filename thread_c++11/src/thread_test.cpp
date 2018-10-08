d7873a22261b3ebecde5616ae802c75a1985d139
Yunfeng Hu <yhu@synaptics.com>
2018-10-07 23:05:30 -0700

    update

--- thread_c++11/src/thread_test.cpp
+++ thread_c++11/src/thread_test.cpp
@@ -1,20 +1,33 @@
-﻿#include <thread>  
-#include <chrono>
-#include <iostream>
-#include<array> 
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
 using namespace std;
 
-void show()
+mutex mtx;
+
+void func(const char *name, int id)
 {
-    cout << "hello cplusplus!" << endl;
+    
+    int cnt = 5;
+    while (cnt--)
+    {
+        mtx.lock();
+        cout << "Thread " << name << "id# " << id << " is running" << endl;
+        mtx.unlock();
+    }
+
 }
+
 int main()
 {
-    array<thread, 3>  threads = { thread(show), thread(show), thread(show) };
-    for (int i = 0; i < 3; i++)
-    {
-        cout << threads[i].joinable() << endl;
-        threads[i].join();
-    }
+    auto n = thread::hardware_concurrency();
+    cout << n << endl;
+
+    thread t1(func, "SPU", 0);
+    thread t2(func, "QTU", 1);
+
+    t1.join();
+    t2.join();
+
     return 0;
 }
